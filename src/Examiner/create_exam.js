b3a31b62e552e5c2f9a08cab15960f4b7d17a21c
jasmine-nahrain <ja5m1ne@hotmail.com>
2020-09-09 15:06:12 +1000

    mostly works. waiting on database to be connected

--- src/Examiner/create_exam.js
+++ src/Examiner/create_exam.js
@@ -8,7 +8,7 @@ import logo from '../images/logo.png';
 import {getUserID} from '../functions.js'
 
 const Body = styled.body`
-  background-color: white;
+  background-color: rgba(0,0,0,0);
   background-blend-mode: multiply;
   min-height: 100vh;
   display: flex;
@@ -37,16 +37,21 @@ class CreateExam extends Component {
     this.onChangeEndDate = this.onChangeEndDate.bind(this);
     this.onChangeStartTime = this.onChangeStartTime.bind(this);
     this.onChangeEndTime = this.onChangeEndTime.bind(this);
+    this.onChangeSubjectID = this.onChangeSubjectID.bind(this);
+    this.onChangeDuration = this.onChangeDuration.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
 
     this.state = {
       name: '',
       start_date: Date,
       end_date: Date,
-      start_time: Date,
+      start_time: new Date(),
       end_time: Date,
       exam_id: Number,
+      subjectID: Number,
+      duration: Number,
     }
+    console.log(this.state);
   }
 
   onChangeName(e) {
@@ -75,14 +80,76 @@ class CreateExam extends Component {
     });
   }
 
+onChangeSubjectID(e) {
+  this.setState({
+    subjectID: e.target.value
+  });
+}
+onChangeDuration(e) {
+  this.setState({
+    duration: e.target.value
+  });
+}
+
+getDurationTime = () => {
+  var duration_hours = this.state.end_time.split(":")[0] - this.state.start_time.split(":")[0];
+  var duration_minutes = this.state.end_time.split(":")[1] - this.state.start_time.split(":")[1];
+  var duration_year = this.state.end_date.split('-')[0] - this.state.start_date.split('-')[0];
+  var duration_month = this.state.end_date.split('-')[1] - this.state.start_date.split('-')[1];
+  var duration_day = this.state.end_date.split('-')[2] - this.state.start_date.split('-')[2];
+
+  if(duration_minutes < 0) {
+    duration_hours--;
+    duration_minutes = 60 + duration_minutes;
+    if(duration_minutes > 60) {
+      duration_minutes -= 60;
+      duration_hours++;
+    }
+  }
+
+  if(duration_hours < 0) {
+    duration_hours += 24;
+    duration_day--;
+  }
+
+  if(duration_hours < 10) {
+    duration_hours = "0" + duration_hours;
+  }
+  if(duration_minutes < 10) {
+    duration_minutes = "0" + duration_minutes;
+  }
+
+  if(duration_year == 0 && duration_month == 0 && duration_day < 0) {
+    alert("Invalid time. End time cannot be before start time.");
+    this.setState({end_date: ''});
+    this.setState({end_time: ''});
+  }
+  if(duration_year == 0 && duration_month == 0 && duration_day == 0 && duration_hours < 0) {
+      alert("Invalid time. End time cannot be before start time.");
+      this.setState({end_time: ''});
+  }
+  return duration_year + "-" + duration_month + "-" + duration_day + " " + duration_hours + ":" + duration_minutes;
+}
+
   onSubmit = async (e) => {
     e.preventDefault();
+    const duration = this.getDurationTime();
+    const newExam = {
+      exam_name: this.state.name,
+      subject_id: this.state.subjectID,
+      start_date: this.state.start_date + " " + this.state.start_time,
+      start_date: this.state.end_date + " " + this.state.end_time,
+      duration: duration,
+    }
+
+    console.log(newExam);
   }
 
   render() {
     // const admin_id = getUserID(false);
     // const is_admin = parseInt(localStorage.getItem('is_admin'));
     // if (admin_id && is_admin) {
+    const today = new Date().toISOString().split("T")[0];
     return (
       <BrowserRouter>
         <div className="App">
@@ -92,13 +159,13 @@ class CreateExam extends Component {
                 <Text>Create a New Exam</Text>
               </Title>
 
-              <Col style={{marginRight: "2%"}}>
+              <Col>
                 <Form.Group controlId="formName">
                   <Form.Control type="text" name="name" placeholder="Exam Name" value={this.state.name} onChange={this.onChangeName} required />
                 </Form.Group>
 
                 <Form.Group controlId="formName">
-                  <Form.Control as="textarea" name="name" placeholder="Exam Description" rows="3" value={this.state.name} onChange={this.onChangeName} required />
+                  <Form.Control type="number" name="name" placeholder="Subject ID" value={this.state.subjectID} onChange={this.onChangeSubjectID} required />
                 </Form.Group>
               </Col>
 
@@ -106,22 +173,54 @@ class CreateExam extends Component {
                 <Row>
                   <Form.Group controlId="formStartDate" style={{fontSize: '16px', marginRight: '10px'}}>
                     <Form.Label>Start Date</Form.Label>
-                    <Form.Control type="date" name="start_date" placeholder="Start Date" value={this.state.start_date} onChange={this.onChangeStartDate} required />
+                    <Form.Control
+                      type="date"
+                      min={today}
+                      name="start_date"
+                      placeholder="Start Date"
+                      dateFormat="yyyy/MM/dd"
+                      value={this.state.start_date}
+                      onChange={this.onChangeStartDate}
+                      required />
                   </Form.Group>
                   <Form.Group controlId="formStartDate" style={{fontSize: '16px', marginLeft: '10px'}}>
                     <Form.Label>Start Time</Form.Label>
-                    <Form.Control type="time" name="start_date" placeholder="Start Date" value={this.state.start_date} onChange={this.onChangeStartTime} required />
+                    <Form.Control
+                      type="time"
+                      name="start_date"
+                      placeholder="Start Date"
+                      dateFormat="hh:mm"
+                      value={this.state.start_time}
+                      locale="aest"
+                      selected={this.state.startDate}
+                      onChange={this.onChangeStartTime}
+                      required />
                   </Form.Group>
 
                 </Row>
                 <Row>
                   <Form.Group controlId="formStudentID" style={{fontSize: '16px', marginRight: '10px'}}>
                     <Form.Label>End Date</Form.Label>
-                    <Form.Control type="date" name="end_date" placeholder="End Date" value={this.state.end_date} onChange={this.onChangeEndDate} required />
+                    <Form.Control
+                      type="date"
+                      name="end_date"
+                      min={this.state.start_date}
+                      placeholder="End Date"
+                      dateFormat="yyyy/MM/dd"
+                      value={this.state.end_date}
+                      onChange={this.onChangeEndDate}
+                      required />
                   </Form.Group>
                   <Form.Group controlId="formStartDate" style={{fontSize: '16px', marginLeft: '10px'}}>
                     <Form.Label>End Time</Form.Label>
-                    <Form.Control type="time" name="start_date" placeholder="Start Date" value={this.state.start_date} onChange={this.onChangeEndTime} required />
+                    <Form.Control
+                      type="time"
+                      name="start_date"
+                      placeholder="Start Date"
+                      dateFormat="hh:mm"
+                      value={this.state.end_time}
+                      onChange={this.onChangeEndTime}
+                      required />
                   </Form.Group>
                 </Row>
               </Col>

81cdb3c6bb391e674d282d8cd7000c7bd2055552
alexadonov <alexadonovan4427@gmail.com>
2020-09-30 01:18:28 +1000

    init userInfo file

---
+++ src/userInfo.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+
+export const register = async newUser => {
+    try {
+        const res = await axios.post('/register', {
+            user_id: newUser.user_id,
+            is_examiner: newUser.is_examiner,
+            first_name: newUser.first_name,
+            last_name: newUser.last_name,
+            password: newUser.password,
+            confirm_examiner: newUser.confirm_examiner
+        });
+      
+        //console.log(res);
+        const status = await res.status;
+        return status === 201;
+    }
+    catch (error) {
+        console.log(error);
+        //alert(`An error occured: "${error}"`);
+        return false;
+    }
+}
+
+export const login = async user => {
+    try {
+        const res = await axios.post('login', {
+            user_id: user.user_id,
+            password: user.password
+        });
+        
+        const status = await res.status;
+        if (status === 200) {
+            localStorage.setItem('token', res.data.token);
+            if (res.data.is_examiner) localStorage.setItem('examiner_id', res.data.user_id);
+            else localStorage.setItem('user_id', res.data.user_id);
+            localStorage.setItem('is_examiner', res.data.is_examiner);
+        }
+        return status === 200;
+    }
+    catch (error) {
+        console.log(error);
+        //alert(`An error occured: "${error}"`);
+        return false;
+    }
+}

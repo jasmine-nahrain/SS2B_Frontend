a74faec616940bc2f54ec61a32ded5a7431d9bc2
jasmine-nahrain <ja5m1ne@hotmail.com>
2020-10-09 16:51:24 +1100

    got it almost working

--- src/Examinee/exam_page.js
+++ src/Examinee/exam_page.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from '../images/logo.png';
+import logo from '../images/logo_white.png';
 import CountDownTimer from './timer.js'
 import styled from 'styled-components';
 import './exampage.css'
-
+import './scripts/script.js';
+import {setupNewBroadcastButtonClickHandler} from './scripts/script.js';
 const Vid = styled.div`
-  position: fixed;
-  top: 0;
-  width: 100%;
+  /* position: fixed;
+  /* top: 0; */
+  /* width: 100%;
   z-index: 100;
-  height: 100%;
+  height: 100%;  */
+`;
+
+const Header = styled.header`
+background-color: #2196f3;
+color: white;
+padding: 1%;
+margin-bottom: 3%;
 `;
 
 class ExamPage extends React.Component {
@@ -20,33 +28,52 @@ class ExamPage extends React.Component {
   };
 
   handleToggle = () => {
+    setupNewBroadcastButtonClickHandler();
     this.setState({
       isActive: !this.state.isActive,
     });
     this.btnReview.setAttribute("disabled", "disabled");
   };
 
+
   render() {
     return (
-      <div>
-        <img src={logo} className="move" alt="logo" />
-        <h1 className = "Something"><b>Examination Page</b></h1>
+      <div className="App">
+        <Header >
+          <img src={logo} className="move" alt="logo" style={{marginLeft: 'auto', marginRight: 'auto'}}/>
+          <h1>Examination Page</h1>
+        </Header>
         <Vid>
           <iframe width="500" height="300" frameBorder="1">
           </iframe>
           <br/><br/><br/>
-          {this.state.isActive ? <CountDownTimer /> : null}
-            <button
-            type="button"
-            class="btn btn-warning"
-            onClick={this.handleToggle}
-            ref={btnReview => {
-              this.btnReview = btnReview;
-            }}
-            >
-            Start Exam
-            </button>
-        </Vid>   
+
+
+        <section class="experiment">
+          <div id="createBroadcast">
+            <section>
+              <input type="text" id="user_id" value={localStorage.getItem('user_id')} disabled/>
+              {this.state.isActive ? <CountDownTimer /> : null}
+                <button
+                type="button"
+                id="setup-new-broadcast"
+                class="btn btn-warning"
+                onClick={this.handleToggle}
+                ref={btnReview => {
+                  this.btnReview = btnReview;
+                }}
+                disabled={this.state.isActive}
+                >
+                Start Exam
+                </button>
+            </section>
+          </div>
+          <div id="listStudents">
+            <table style={{width: '100%'}} id="rooms-list"></table>
+          </div>
+          <div id="videos-container"></div>
+        </section>
+        </Vid>
       </div>
     );
   }
---
+++ src/Examinee/scripts/script.js
@@ -0,0 +1,192 @@
+import "detectrtc/DetectRTC.js";
+import io from "socket.io-client";
+// import './scripts/adapter-latest.js';
+import "./IceServersHandler.js";
+import "./CodecsHandler.js";
+import "rtcpeerconnection/rtcpeerconnection.js";
+import { broadcast } from "./broadcast.js";
+var DetectRTC = require('detectrtc');
+
+document.createElement("article");
+document.createElement("footer");
+// const queryString = window.location.search;
+// const urlParams = new URLSearchParams(queryString);
+//currently variables being used are passed using urlparams, this will need to be queried via the api. variable names examId, studentId and boolean examiner)
+
+var config = {
+  openSocket: function (config) {
+    console.log(new Date());
+    var SIGNALING_SERVER = 'http://localhost:3000'; //"https://socketio-over-nodejs2.herokuapp.com:443/";
+
+    config.channel = config.channel || localStorage.getItem("exam_id"); //examId signifies exam being taken/exam being viewed
+    console.log(config.channel);
+
+    // document.getElementById('user_id').value = localStorage.getItem('user_id').toString();
+
+    //show different ui based on examiner/examinee.
+
+    if (localStorage.getItem("is_examiner") === 1)
+      document.getElementById("createBroadcast").style.display = "none";
+    if (localStorage.getItem("is_examiner") === 0)
+      document.getElementById("listStudents").style.display = "none";
+    var sender = Math.round(Math.random() * 999999999) + 999999999;
+
+    io.connect(SIGNALING_SERVER).emit("new-channel", {
+      channel: config.channel,
+      sender: sender,
+    });
+
+    var socket = io.connect(SIGNALING_SERVER);
+    socket.channel = config.channel;
+    socket.on("connect", function () {
+      if (config.callback) config.callback(socket);
+    });
+
+    socket.send = function (message) {
+      socket.emit("message", {
+        sender: sender,
+        data: message,
+      });
+    };
+
+    socket.on("message", config.onmessage);
+  },
+  onRemoteStream: function (htmlElement) {
+    videosContainer.appendChild(htmlElement);
+  },
+  onRoomFound: function (room) {
+    var alreadyExist = document.querySelector(
+      'button[data-broadcaster="' + room.broadcaster + '"]'
+    );
+    if (alreadyExist) return;
+
+    if (typeof roomsList === "undefined") roomsList = document.body;
+
+    var tr = document.createElement("tr");
+    tr.innerHTML =
+      "<td><strong>" +
+      room.roomName +
+      "</strong> is broadcasting his media!</td>" +
+      '<td><button class="join">Join</button></td>';
+    roomsList.appendChild(tr);
+
+    var joinRoomButton = tr.querySelector(".join");
+    joinRoomButton.setAttribute("data-broadcaster", room.broadcaster);
+    joinRoomButton.setAttribute("data-roomToken", room.broadcaster);
+    joinRoomButton.onclick = function () {
+      //this.disabled = true;
+
+      var broadcaster = this.getAttribute("data-broadcaster");
+      var roomToken = this.getAttribute("data-roomToken");
+      broadcastUI.joinRoom({
+        roomToken: roomToken,
+        joinUser: broadcaster,
+      });
+      hideUnnecessaryStuff();
+    };
+  },
+  onNewParticipant: function (numberOfViewers) {
+    document.title = "Viewers: " + numberOfViewers;
+  },
+  onReady: function () {
+    console.log("now you can open or join rooms");
+  },
+};
+
+export function setupNewBroadcastButtonClickHandler() {
+  const studentId = localStorage.getItem("user_id");
+  console.log(studentId.toString());
+  // document.getElementById("setup-new-broadcast").disabled = true;
+
+  DetectRTC.load(function () {
+    captureUserMedia(function () {
+      var shared = "video";
+      broadcastUI.createRoom({
+        roomName:
+          (document.getElementById("user_id") || {}).value || "Anonymous",
+        isAudio: shared === "audio",
+      });
+    });
+    hideUnnecessaryStuff();
+  });
+  console.log(DetectRTC);
+}
+
+function captureUserMedia(callback) {
+  console.log(new Date());
+  var constraints = null;
+  var option = broadcastingOption ? broadcastingOption.value : "";
+
+  if (
+    option != "Only Audio" &&
+    option != "Screen" &&
+    DetectRTC.hasWebcam !== true
+  ) {
+    alert(
+      "DetectRTC library is unable to find webcam; maybe you denied webcam access once and it is still denied or maybe webcam device is not attached to your system or another app is using same webcam."
+    );
+  }
+
+  var htmlElement = document.createElement(
+    option === "Only Audio" ? "audio" : "video"
+  );
+
+  htmlElement.muted = true;
+  htmlElement.volume = 0;
+
+  try {
+    htmlElement.setAttributeNode(document.createAttribute("autoplay"));
+    htmlElement.setAttributeNode(document.createAttribute("playsinline"));
+    htmlElement.setAttributeNode(document.createAttribute("controls"));
+  } catch (e) {
+    htmlElement.setAttribute("autoplay", true);
+    htmlElement.setAttribute("playsinline", true);
+    htmlElement.setAttribute("controls", true);
+  }
+
+  var mediaConfig = {
+    video: htmlElement,
+    onsuccess: function (stream) {
+      config.attachStream = stream;
+
+      videosContainer.appendChild(htmlElement);
+
+      callback && callback();
+    },
+    onerror: function () {
+      if (option === "Only Audio")
+        alert("unable to get access to your microphone");
+      else if (option === "Screen") {
+        if (window.location.protocol === "http:")
+          alert("Please test this WebRTC experiment on HTTPS.");
+        else
+          alert(
+            'Screen capturing is either denied or not supported. Are you enabled flag: "Enable screen capture support in getUserMedia"?'
+          );
+      } else alert("unable to get access to your webcam");
+    },
+  };
+  if (constraints) mediaConfig.constraints = constraints;
+  navigator.mediaDevices.getUserMedia(mediaConfig);
+}
+
+var broadcastUI = broadcast(config);
+
+/* UI specific */
+var videosContainer =
+  document.getElementById("videos-container") || document.body;
+var setupNewBroadcast = document.getElementById("setup-new-broadcast");
+var roomsList = document.getElementById("rooms-list");
+
+var broadcastingOption = document.getElementById("broadcasting-option");
+
+if (setupNewBroadcast)
+  setupNewBroadcast.onclick = setupNewBroadcastButtonClickHandler();
+
+function hideUnnecessaryStuff() {
+  var visibleElements = document.getElementsByClassName("visible"),
+    length = visibleElements.length;
+  for (var i = 0; i < length; i++) {
+    visibleElements[i].style.display = "none";
+  }
+}

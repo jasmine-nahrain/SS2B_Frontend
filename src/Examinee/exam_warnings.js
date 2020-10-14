339203c68fe6bb5ce182b2a4d6f8182f2ca35207
justinsaji <justinsaji1@gmail.com>
2020-10-14 18:29:12 +1100

    Create exam warning part WIP

--- src/Examinee/exam_warnings.js
+++ src/Examinee/exam_warnings.js
@@ -6,18 +6,10 @@ import styled from 'styled-components';
 import { BrowserRouter } from "react-router-dom";
 import { getExamWarning, deleteExamWarning } from '../api_caller';
 import { formatDateToLocal } from '../functions.js';
-
-const Body = styled.body`
-  background-color: white;
-  background-blend-mode: multiply;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: black;
-`;
+import DateTime from 'react-datetime';
+import "react-datetime/css/react-datetime.css";
+import { getLatestEndTime } from '../functions.js';
+import moment from 'moment';
 
 const Title = styled.div`
     padding:14px 5px 14px 0px;
@@ -31,16 +23,36 @@ export default class ExamWarnings extends Component {
 
     constructor(props) {
         super(props);
+        // this.getExamWarnings = this.getExamWarnings.bind(this);
+        // var offset = - (new Date()).getTimezoneOffset();
+        // let time_started = (new Date(props.data.time_started));
+        // time_started.setMinutes(time_started.getMinutes() + offset)
+        // let latest_time_ended = getLatestEndTime(props.data.time_started, props.data.duration);
+
         this.state = {
-            exam_recording_id: props.exam_recording_id,
+            exam_recording_id: props.data.exam_recording_id,
             exam_warnings: [],
-            is_examiner: props.is_examiner
+            is_examiner: props.data.is_examiner === true || props.data.is_examiner === 1,
+            time_started: "2020-10-14 18:11:00",
+            latest_time_ended: "2020-10-14 21:11:00",
+            create_new: false,
+            new_description: '',
+            new_warning_time: null
         }
+        console.log(moment(this.state.time_started).format('YYYY-MM-DD hh:mm:ss'))
+        console.log(moment(this.state.latest_time_ended).format('YYYY-MM-DD hh:mm:ss'))
     }
 
-    async componentDidMount() {
-        // Gets data before the render
+    componentDidMount = async () => {
+        // change to 30000
         await this.getExamWarnings();
+        if (!this.state.is_examiner) this.interval = setInterval(async () => {
+            await this.getExamWarnings();
+        }, 30000);
+    }
+
+    componentWillUnmount() {
+        if (!this.state.is_examiner) clearInterval(this.interval);
     }
 
     onSubmit = async (e) => {
@@ -48,34 +60,105 @@ export default class ExamWarnings extends Component {
     }
 
     getExamWarnings = async () => {
-        let exam_warning_data = getExamWarning({ 'exam_recording_id': this.state.exam_recording_id });
-        this.setState({
-            exam_warnings: exam_warning_data.exam_warnings
-        });
+        let exam_warning_data = await getExamWarning({ 'exam_recording_id': this.state.exam_recording_id });
+        console.log(exam_warning_data);
+        if (exam_warning_data !== null && exam_warning_data.exam_warnings.length > 0) {
+            this.setState({
+                exam_warnings: exam_warning_data.exam_warnings
+            }, () => { console.log(this.state.exam_warnings) });
+        }
+
     }
 
     deleteSingleExamWarning = async (exam_warning_id) => {
         let success = deleteExamWarning(exam_warning_id);
         if (!success) {
             alert('The exam warning could not be deleted. Please try again.');
-        }
+        } else await this.getExamWarnings();
+    }
+
+    createSingleWarning = async () => {
+
+    }
+
+    allowCreate = () => {
+        this.setState({
+            create_new: true
+        });
+    }
+
+    cancelCreate = () => {
+        this.setState({
+            create_new: false,
+            new_description: '',
+            new_warning_time: null
+        });
+    }
+
+    onChangeNewDescription = (e) => {
+        e.preventDefault();
+        this.setState({
+            new_description: e.target.value
+        }, () => { console.log(this.state.new_description) });
+    }
+
+    onChangeNewWarningTime = (datetime) => {
+        this.setState({
+            new_warning_time: datetime._d
+        }, () => { console.log(this.state.new_warning_time) });
+    }
+
+    isValidDateTime = (current) => {
+
+        return current.isAfter(moment(this.state.time_started).format('YYYY-MM-DD hh:mm:ss'))
+            && current.isBefore(moment(this.state.latest_time_ended).format('YYYY-MM-DD hh:mm:ss'))
     }
 
     render() {
         return (
             <div>
-                <Body>
-                    {this.state.exam_warnings.map((warning) =>
-                        <div>
-                            <Alert key={warning.exam_warning_id} variant="danger">{formatDateToLocal(warning.warning_time)}: {warning.descriptin}</Alert>
-                            {this.state.is_examiner &&
-                                <div>
-                                    <Button onClick={()=>this.deleteSingleExamWarning(warning.exam_warning_id)} >Delete</Button>
+                {this.state.exam_warnings.map((warning) =>
+                    <Alert key={warning.exam_warning_id} variant="danger">
+                        {!this.state.is_examiner &&
+                            <p>{formatDateToLocal(warning.warning_time)}: {warning.description}</p>
+                        }
+                        {this.state.is_examiner &&
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-10">
+                                        <p>{formatDateToLocal(warning.warning_time)}: {warning.description}</p>
+                                    </div>
+                                    <div class="col">
+                                        <button type="button" class="btn btn-lg btn-danger" onClick={() => this.deleteSingleExamWarning(warning.exam_warning_id)} >Delete</button>
+                                    </div>
                                 </div>
-                            }
-                        </div>
-                    )}
-                </Body>
+                            </div>
+                        }
+                    </Alert>
+                )}
+                {this.state.is_examiner &&
+                    <div>
+                        <button type="button" hidden={this.state.create_new} onClick={this.allowCreate} class="btn btn-lg btn-block btn-danger">Give Warning</button>
+                        {this.state.create_new &&
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col">
+                                        <DateTime isValidDate={this.isValidDateTime} initialValue={new Date()} onChange={this.onChangeNewWarningTime} value={this.state.new_warning_time} />
+                                        <input onChange={this.onChangeNewDescription} placeholder="Enter warning description here." value={this.state.new_description} ></input>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <button class="btn btn-lg btn-block btn-primary">Save</button>
+                                        <button class="btn btn-lg btn-block btn-secondary" onClick={this.cancelCreate}>Cancel</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                        }
+                    </div>
+                }
+
             </div>
         );
     }

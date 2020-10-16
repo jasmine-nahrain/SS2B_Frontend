841a01e391486f1d516f6bcfab25a04ebef66708
justinsaji <justinsaji1@gmail.com>
2020-10-16 21:19:21 +1100

    Updated warning date time fields

--- src/Examinee/exam_warnings.js
+++ src/Examinee/exam_warnings.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Form, Button, Alert } from 'react-bootstrap';
+import { Form, Button, Alert, Col } from 'react-bootstrap';
 import styled from 'styled-components';
 import { BrowserRouter } from "react-router-dom";
 import { getExamWarning, createExamWarning, deleteExamWarning } from '../api_caller';
@@ -32,12 +32,15 @@ export default class ExamWarnings extends Component {
         this.toggleCreate = this.toggleCreate.bind(this);
         this.deleteSingleExamWarning = this.deleteSingleExamWarning.bind(this);
         this.createSingleWarning = this.createSingleWarning.bind(this);
-        this.onChangeNewDescription = this.onChangeNewDescription.bind(this);
-        this.onChangeNewWarningTime = this.onChangeNewWarningTime.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeWarningTime = this.onChangeWarningTime.bind(this);
+        this.onChangeWarningDate = this.onChangeWarningDate.bind(this);
         this.setExamWarnings = this.setExamWarnings.bind(this);
 
-        let time_started_string = this.props.data.time_started;
-        let time_started = moment.utc(time_started_string, datetimeformat).toDate()
+        let m = new Date(this.props.data.time_started);
+        m.setMinutes(m.getMinutes() - (new Date()).getTimezoneOffset());
+        let time_started_string = moment(m).format(datetimeformat).split(' ');
+        
         let duration = this.props.data.duration;
         let latest_time_ended = this.props.data.time_ended;
         if (latest_time_ended === null || !latest_time_ended) latest_time_ended = getLatestEndTime(time_started_string, duration);
@@ -46,11 +49,12 @@ export default class ExamWarnings extends Component {
             exam_recording_id: props.data.exam_recording_id,
             exam_warnings: [],
             is_examiner: props.data.is_examiner,
-            time_started: time_started,
+            time_started: time_started_string,
             latest_time_ended: latest_time_ended,
             create_new: false,
             new_description: '',
-            new_warning_time: time_started
+            new_warning_time: time_started_string[1],
+            new_warning_date: time_started_string[0]
         }
     }
 
@@ -68,13 +72,13 @@ export default class ExamWarnings extends Component {
 
     getExamWarnings = async () => {
         let exam_warning_data = await getExamWarning({ 'exam_recording_id': this.state.exam_recording_id });
-        console.log("all warnings:",exam_warning_data);
+        console.log("all warnings:", exam_warning_data);
         if (exam_warning_data !== null && exam_warning_data.exam_warnings.length > 0) {
             await this.setExamWarnings(exam_warning_data.exam_warnings);
         }
     }
 
-    setExamWarnings = async(warnings) => {
+    setExamWarnings = async (warnings) => {
         this.setState({
             exam_warnings: warnings
         });
@@ -92,18 +96,11 @@ export default class ExamWarnings extends Component {
     }
 
     createSingleWarning = async () => {
-        if (!this.isValidDateTime(moment(this.state.new_warning_time))) {
-            alert("The warning time is required to be between exam time started and exam time ended (or current time).");
-            return;
-        }
+        var warning_time = new Date(this.state.new_warning_date + " " + this.state.new_warning_time)
+        warning_time.setMinutes(warning_time.getMinutes() + (new Date()).getTimezoneOffset());
+        let warning_time_string = moment(warning_time).format(datetimeformat)
 
-        if (this.state.new_warning_time === null || this.state.new_description === '' || this.state.new_description === null) {
-            alert("Please enter a valid warning time and warning description.");
-            return;
-        }
-
-        var warning_time = moment.utc(this.state.new_warning_time).format(datetimeformat);
-        var new_warning = createExamWarning(this.state.exam_recording_id, warning_time, this.state.new_description)
+        var new_warning = createExamWarning(this.state.exam_recording_id, warning_time_string, this.state.new_description)
         if (new_warning === null) alert('Something went wrong!');
         else {
             setTimeout(async () => {
@@ -111,6 +108,7 @@ export default class ExamWarnings extends Component {
             }, 250);
             this.toggleCreate();
         }
+        
     }
 
     toggleCreate = () => {
@@ -120,77 +118,104 @@ export default class ExamWarnings extends Component {
         });
     }
 
-    onChangeNewDescription = async(e) => {
+    onChangeDescription = async (e) => {
         e.preventDefault();
         this.setState({
             new_description: e.target.value
         });
     }
 
-    onChangeNewWarningTime = (datetime) => {
+    onChangeWarningTime = (e) => {
+        e.preventDefault()
         this.setState({
-            new_warning_time: datetime._d
+            new_warning_time: e.target.value
         });
     }
 
-    isValidDateTime = (current) => {
-        return current.isAfter(moment(this.state.time_started).subtract(1, 'minute'))
-            && current.isBefore(moment(this.state.latest_time_ended).add(1, 'minute'))
-    }
-
-    valid = (current) => {
-        return current.isAfter(moment().subtract(2, 'day')) && current.isBefore(moment().add(2, 'day'))
+    onChangeWarningDate = (e) => {
+        e.preventDefault()
+        this.setState({
+            new_warning_date: e.target.value
+        });
     }
 
     render() {
         return (
-            <div style={{width: '30%', marginLeft: 'auto', marginRight: 'auto'}}>
+            <div style={{ width: '30%', marginLeft: 'auto', marginRight: 'auto' }}>
                 {this.state.is_examiner &&
                     <div class="my-3">
                         <button type="button" hidden={this.state.create_new} onClick={this.toggleCreate} class="btn btn-lg btn-block btn-danger">Give Warning</button>
                         {this.state.create_new &&
                             <div class="container">
+                                <Form>
+                                    <Form.Row>
+                                        <Col>
+                                        
+                                            <Form.Control
+                                                type="date" min={this.state.new_warning_date} name="warning_date" placeholder="Warning Date"
+                                                value={this.state.new_warning_date} onChange={this.onChangeWarningDate} required />
+                                        </Col>
+                                        <Col>
+                                            <Form.Control
+                                                type="time" name="warning_time" placeholder="Warning Time" dateFormat="hh:mm"
+                                                value={this.state.new_warning_time} selected={this.state.new_warning_time}
+                                                onChange={this.onChangeWarningTime} required />
+                                        </Col>
+                                    </Form.Row>
+
+                                    <Form.Row>
+                                        <Col>
+                                            <Form.Control
+                                                type="text" name="warning_description" placeholder="Warning Description"
+                                                value={this.state.new_description}
+                                                onChange={this.onChangeDescription} required />
+                                        </Col>
+                                    </Form.Row>
+
+                                    <Form.Row>
+                                        <Col>
+                                            <button class="btn btn-lg btn-block btn-danger" onClick={this.createSingleWarning}>Save</button>
+                                            <button class="btn btn-lg btn-block btn-secondary" onClick={this.toggleCreate}>Cancel</button>
+                                        </Col>
+                                    </Form.Row>
+                                </Form>
                                 <div class="row my-2">
-                                    <div class="col">
-                                        <DateTime isValidDate={this.valid} initialValue={this.state.time_started} onChange={this.onChangeNewWarningTime} value={this.state.new_warning_time} />
-                                    </div>
-                                    <div class="col-9">
-                                        <input onChange={this.onChangeNewDescription} placeholder="Enter warning description here." value={this.state.new_description} ></input>
-                                    </div>
                                 </div>
                                 <div class="row">
                                     <div class="col">
-                                        <button class="btn btn-lg btn-block btn-danger" onClick={this.createSingleWarning}>Save</button>
-                                        <button class="btn btn-lg btn-block btn-secondary" onClick={this.toggleCreate}>Cancel</button>
+
                                     </div>
                                 </div>
+
                             </div>
 
                         }
                     </div>
                 }
-                {this.state.exam_warnings.map((warning) =>
-                    <Alert key={warning.exam_warning_id} variant="danger">
-                        {!this.state.is_examiner &&
-                            <p>{formatDateToLocal(warning.warning_time)}: {warning.description}</p>
-                        }
-                        {this.state.is_examiner &&
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-10">
-                                        <p>{formatDateToLocal(warning.warning_time)}: {warning.description}</p>
-                                    </div>
-                                    <div class="col">
-                                        <button type="button" class="btn btn-lg btn-danger" onClick={() => this.deleteSingleExamWarning(warning.exam_warning_id)} >Delete</button>
+                {
+                    this.state.exam_warnings.map((warning) =>
+                        <Alert key={warning.exam_warning_id} variant="danger">
+                            {!this.state.is_examiner &&
+                                <p>{formatDateToLocal(warning.warning_time)}: {warning.description}</p>
+                            }
+                            {this.state.is_examiner &&
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-10">
+                                            <p>{formatDateToLocal(warning.warning_time)}: {warning.description}</p>
+                                        </div>
+                                        <div class="col">
+                                            <button type="button" class="btn btn-lg btn-danger" onClick={() => this.deleteSingleExamWarning(warning.exam_warning_id)} >Delete</button>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        }
-                    </Alert>
-                )}
+                            }
+                        </Alert>
+                    )
+                }
 
 
-            </div>
+            </div >
         );
     }
 }
